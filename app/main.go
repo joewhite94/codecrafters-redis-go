938009adbedda60ca33ad4efb48ed5aafbcb7926
Joe White <joe.white@cudoventures.com>
2026-03-16 20:26:43 +0000

    codecrafters submit [skip ci]

--- app/main.go
+++ app/main.go
@@ -12,18 +12,24 @@ func main() {
 		fmt.Println("Failed to bind to port 6379")
 		os.Exit(1)
 	}
-	conn, err := l.Accept()
-	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
-		os.Exit(1)
-	}
 
+	for {
+		conn, err := l.Accept()
+		if err != nil {
+			fmt.Println("Error accepting connection: ", err.Error())
+			os.Exit(1)
+		}
+		go handleConnection(conn)
+	}
+}
+
+func handleConnection(conn net.Conn) error {
 	buf := make([]byte, 1024)
 
 	for {
-		_, err = conn.Read(buf)
+		_, err := conn.Read(buf)
 		if err != nil {
-			break
+			return err
 		}
 		conn.Write([]byte("+PONG\r\n"))
 	}
